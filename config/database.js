6671a7e1e41439c98eb53ae7df1c55714ddf1578
Carlos Acevedo <cacevedo@brandinstitute.com>
2017-11-09 12:36:28 -0500

    deploying to heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://carlos:19994251@ds151955.mlab.com:51955/resume',
+    database: 'mongodb://carlos:carlos@ds151955.mlab.com:51955/resume',
     // database: 'mongodb://localhost:27017/resumedata',
     secret: 'yoursecret'
 }
